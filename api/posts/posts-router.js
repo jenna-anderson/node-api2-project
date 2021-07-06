a573135d2692f492e23fc1d0e4314f5fd359642b
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-06 15:38:20 -0600

    completes PUT request updating and returning updated post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -39,25 +39,7 @@ router.get('/:id', (req, res) => {
     })
 })
 
-// [POST] creates post and returns id of newly created post
-// router.post('/', (req, res) => {
-//     const { title, contents } = req.body;
-//     Posts.insert({ title, contents })
-//     .then(id => {
-//         const newPostId = id
-//         Posts.findById(newPostId)
-//         .then(post => {
-//             console.log(post)
-//         })
-        
-//     })
-//     .catch(err => {
-//         res.status(500).json({
-//             message: "There was an error while saving the post to the database"
-//         })
-//     })
-//  })
-
+// [POST] creates post and returns newly created post
 router.post('/', async (req, res) => {
     const { title, contents } = req.body;
     if (!title || !contents) {
@@ -76,5 +58,32 @@ router.post('/', async (req, res) => {
         }
     }
  })
+
+// [PUT] updates post by id and returns updated post
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        try {
+            const count = await Posts.update(id, { title, contents })
+            if (count == 1) {
+                const updatedPost = await Posts.findById(id)
+                res.status(200).json(updatedPost);
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            }
+        } catch (err) {
+            res.status(500).json({
+                message: "The post information could not be modified"
+            })
+        }
+    }
+})
 
 module.exports = router;
