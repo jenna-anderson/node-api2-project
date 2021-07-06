0b7386182e8ccd883cd72d538253223b2cae1219
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-06 14:02:31 -0600

    completes GET request to fetch all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,22 @@
 // implement your posts router here
+const express = require('express');
+const Posts = require('./posts-model');
+
+const router = express.Router();
+
+// ENDPOINTS
+
+// [GET] 
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved"
+        })
+    })
+})
+
+module.exports = router;
