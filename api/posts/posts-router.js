3d2bf2e63a077e96e35803e5f47d5718b8150c42
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-06 15:53:27 -0600

    completes GET request fetching array of comments for a post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -106,5 +106,25 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
+// [GET] returns array of all comment objects associated with the post by id
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    Posts.findPostComments(id)
+    .then(comments => {
+        if(!comments.length) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(comments)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+           message: "The comments information could not be retrieved" 
+        })
+    })
+})
+
 
 module.exports = router;
